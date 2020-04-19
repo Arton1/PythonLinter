ed9d669abd1b5d8f819e80774abfe87880e5a6c9
Artur Wyrozębski <01133391@pw.edu.pl>
2020-04-19 15:08:21 +0200

    Better column calculation idea

--- source/StreamHandler.java
+++ source/StreamHandler.java
@@ -5,9 +5,8 @@ public class StreamHandler {
     private Scanner scanner;
     private String line;
     private int currentLinePosition;
-    private int currentColumnPosition;
     private boolean EOF;
-    private boolean EOL;
+    private int initialLineSize;
 
     static final char EOF_CHARACTER = (char)-1;
     static final char EOL_CHARACTER = (char)10;
@@ -32,14 +31,12 @@ public class StreamHandler {
     private void initialize(){
         scanner.useDelimiter("");
         currentLinePosition = 0;
-        currentColumnPosition = 0;
         EOF = false;
         line = null;
     }
 
     public char readCharacter() { 
         if(line == null){ //no line, read next line
-            currentColumnPosition = 0;
             EOF = setCursorAtNextCharacter(); //set cursor at next true character
             if(EOF) //no more lines
                 return EOF_CHARACTER;
@@ -51,24 +48,20 @@ public class StreamHandler {
         }
         char c = line.charAt(0);
         if(c == SPACE_CHARACTER)
-            if(passSpaces()) { //check line contains only spaces and pass any mass spaces
-                currentColumnPosition++; //adding EOL character to line
+            if(passSpaces()) { //check line contains only spaces and pass any multiple spaces
                 return EOL_CHARACTER; //simulate EOL
             }
             else
                 return SPACE_CHARACTER;
         if(c == EOL_CHARACTER){
             line = null;
-            currentColumnPosition++;
             return EOL_CHARACTER;
         }
         line = line.substring(1);
-        currentColumnPosition++;
         return c;
     }
 
     private boolean processEmptyLines(){ //returns EOF status
-        currentColumnPosition = 1;
         do {
             currentLinePosition++;
             if (scanner.hasNextLine())
@@ -83,7 +76,6 @@ public class StreamHandler {
         if(c != SPACE_CHARACTER){
             if(line != null){
                 line = c + line;
-                currentColumnPosition--;
             }
             else
                 line = Character.toString(c);
@@ -93,7 +85,6 @@ public class StreamHandler {
     private boolean passSpaces(){ //EOL status
         while (line.length() != 0 && line.charAt(0) == SPACE_CHARACTER){ //pass all spaces
             line = line.substring(1, line.length());
-            currentColumnPosition++;
         }
         if(line.length() == 0)
             return true; //is EOL, no EOL character
@@ -105,7 +96,10 @@ public class StreamHandler {
     }
 
     public int getCurrentColumnPosition(){
-        return currentColumnPosition;
+        if(line != null)
+            return initialLineSize - line.length();
+        else
+            return initialLineSize+1; //EOL position
     }
 
     public boolean isEOF(){
@@ -117,6 +111,7 @@ public class StreamHandler {
             line = scanner.nextLine(); //load line
         else //EOF status
             return true;
+        initialLineSize = line.length();
         if(line.length() == 0) //empty line
             if(processEmptyLines()) //pass empty lines
                 return true;
