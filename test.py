ad127ce0f305134e5d0df957d2c716f62eb2d7b6
Artur Wyrozębski <01133391@pw.edu.pl>
2020-05-10 23:54:29 +0200

    Add tests

---
+++ test.py
@@ -0,0 +1,12 @@
+from abc import ABC
+import math
+
+class Klasa(ABC):
+	True or False
+	def __init__(self):
+		xIsFive = self._hello(5)
+
+	def _hello(self, x: int) -> bool:
+		if x == 5:
+			return True
+		return False
