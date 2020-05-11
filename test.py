d48a4bef73f7430d4110878bed969dae011594da
Artur Wyrozębski <01133391@pw.edu.pl>
2020-05-11 04:01:58 +0200

    Change main test

--- test.py
+++ test.py
@@ -9,9 +9,11 @@ class Klasa(ABC):
 		xIsFive = self._hello(5)
 		self.print(self.value)
 
-	def func(value1,value2):
-		value1-=-value2 
-		print(value1+value2)
-		return
-
-	pass
+	def funkcja(self, x: int) -> bool:
+		if x == 1:
+			print(x)
+		elif x == 2:
+			x += 2
+		else:
+			return x == 1
+		return True
